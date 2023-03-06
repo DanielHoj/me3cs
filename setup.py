366d470cafa677f39be527d6f4e63b72fa2680d2
DanielHoj <danielhoj1990@gmail.com>
2023-03-06 15:11:20 +0100

    added README.rst and LICENSE.txt

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
 
 setup(
     name='me3cs',
     version='0.1.3',
-    install_requires=["pandas", "numpy", "scipy"],
-    packages=['me3cs', 'me3cs.misc', 'me3cs.metrics', 'me3cs.metrics.regression', 'me3cs.framework',
-              'me3cs.framework.helper_classes', 'me3cs.model_types', 'me3cs.model_types.regression',
-              'me3cs.model_types.decomposition', 'me3cs.missing_data', 'me3cs.preprocessing', 'me3cs.cross_validation'],
-    url='',
-    license='',
+    license='MIT',
     author='Daniel Andreas Njust Høj',
     author_email='d.njust.hoej@gmail.com',
-    description='Chemometrical package for '
+    packages=find_packages('me3cs'),
+    package_dir={'': 'me3cs'},
+    url='https://github.com/DanielHoj/me3cs',
+    keywords='chemometrics',
+    install_requires=["pandas",
+                      "numpy",
+                      "scipy"],
+
 )
+
