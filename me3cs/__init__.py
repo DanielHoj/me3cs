271cbf7f10763f49f7e31d4d055ad83549387810
DanielHoj <danielhoj1990@gmail.com>
2023-03-06 14:27:09 +0100

    added init and changed setup

---
+++ me3cs/__init__.py
@@ -0,0 +1,3 @@
+from .framework.model import Model
+from .preprocessing.preprocessing import Preprocessing
+from .missing_data.core import MissingData
--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(
     name='me3cs',
     version='0.1.3',
+    install_requires=["pandas", "numpy", "scipy"],
     packages=['me3cs', 'me3cs.misc', 'me3cs.metrics', 'me3cs.metrics.regression', 'me3cs.framework',
               'me3cs.framework.helper_classes', 'me3cs.model_types', 'me3cs.model_types.regression',
               'me3cs.model_types.decomposition', 'me3cs.missing_data', 'me3cs.preprocessing', 'me3cs.cross_validation'],
@@ -10,5 +11,5 @@ setup(
     license='',
     author='Daniel Andreas Njust Høj',
     author_email='d.njust.hoej@gmail.com',
-    description='Chemometrical package'
+    description='Chemometrical package for '
 )
