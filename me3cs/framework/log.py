4ce6f7762ddfef2d1c4006d9fb884592f621a5e7
DanielHoj <danielhoj1990@gmail.com>
2023-04-12 14:48:36 +0200

    created log for models

---
+++ me3cs/framework/log.py
@@ -0,0 +1,57 @@
+import datetime
+from copy import deepcopy
+
+from me3cs.framework.branch import Branch
+from me3cs.framework.results import Results
+
+
+class LogObject:
+    def __init__(self, branches: [list[Branch, Branch], list[Branch]], results: Results):
+        self._branches = branches
+        if len(branches) > 0:
+            x, y = branches
+            self.x_prep = x.preprocessing.called
+            self.y_prep = y.preprocessing.called
+            # self.x_missing = x.missing_data.called
+            # self.y_missing = y.missing_data.called
+        else:
+            x = branches
+            self.x_prep = x.preprocessing.called
+            # self.x_missing = x.missing_data.called
+        self.results = results
+        self.rows = x._row_index.get_total_index()
+        self.created_at = datetime.datetime.now().replace(microsecond=0)
+        self.last_model_called = None
+
+    def __repr__(self):
+        return f"model type: {self.last_model_called} created {self.created_at}"
+
+    def __copy__(self):
+        new_obj = LogObject(self._branches, self.results)
+        new_obj.created_at = datetime.datetime.now().replace(microsecond=0)
+        new_obj.last_model_called = self.last_model_called
+        return new_obj
+
+    def __deepcopy__(self, memo):
+        new_obj = LogObject(deepcopy(self._branches, memo), deepcopy(self.results, memo))
+        new_obj.created_at = datetime.datetime.now().replace(microsecond=0)
+        new_obj.last_model_called = self.last_model_called
+        return new_obj
+
+
+class Log:
+    def __init__(self, branches: [list[Branch, Branch], list[Branch]], results: Results):
+        self._x_branch = branches[0]
+        if len(branches) > 0:
+            self._y_branch = branches[0]
+        self._results = results
+        self.model_details = LogObject(branches, results)
+        self.models = []
+
+    def make_log(self):
+        new_log = deepcopy(self.model_details)
+
+        self.models.append(new_log)
+
+    def get_summary(self):
+        pass
