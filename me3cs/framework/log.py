2b38038476ae44d9900666afdec8e4d62e8838bd
DanielHoj <danielhoj1990@gmail.com>
2023-04-13 10:17:32 +0200

    added options to log

--- me3cs/framework/log.py
+++ me3cs/framework/log.py
@@ -1,51 +1,55 @@
 import datetime
 from copy import deepcopy
 
+from me3cs.framework.helper_classes.options import Options
+
 from me3cs.framework.branch import Branch
 from me3cs.framework.results import Results
 
 
 class LogObject:
-    def __init__(self, branches: [list[Branch, Branch], list[Branch]], results: Results):
+    def __init__(self, branches: [list[Branch, Branch], list[Branch]], results: Results, options: Options) -> None:
         self._branches = branches
         if len(branches) > 0:
             x, y = branches
             self.x_prep = x.preprocessing.called
             self.y_prep = y.preprocessing.called
-            # self.x_missing = x.missing_data.called
-            # self.y_missing = y.missing_data.called
+            self.x_missing = x.missing_data.called
+            self.y_missing = y.missing_data.called
         else:
             x = branches
             self.x_prep = x.preprocessing.called
-            # self.x_missing = x.missing_data.called
+            self.x_missing = x.missing_data.called
         self.results = results
+        self.options = options
         self.rows = x._row_index.get_total_index()
         self.created_at = datetime.datetime.now().replace(microsecond=0)
         self.last_model_called = None
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"model type: {self.last_model_called} created {self.created_at}"
 
     def __copy__(self):
-        new_obj = LogObject(self._branches, self.results)
+        new_obj = LogObject(self._branches, self.results, self.options)
         new_obj.created_at = datetime.datetime.now().replace(microsecond=0)
         new_obj.last_model_called = self.last_model_called
         return new_obj
 
     def __deepcopy__(self, memo):
-        new_obj = LogObject(deepcopy(self._branches, memo), deepcopy(self.results, memo))
+        new_obj = LogObject(deepcopy(self._branches, memo), deepcopy(self.results, memo), deepcopy(self.options, memo))
         new_obj.created_at = datetime.datetime.now().replace(microsecond=0)
         new_obj.last_model_called = self.last_model_called
         return new_obj
 
 
 class Log:
-    def __init__(self, branches: [list[Branch, Branch], list[Branch]], results: Results):
+    def __init__(self, branches: [list[Branch, Branch], list[Branch]], results: Results, options: Options):
         self._x_branch = branches[0]
         if len(branches) > 0:
             self._y_branch = branches[0]
         self._results = results
-        self.model_details = LogObject(branches, results)
+        self.options = options
+        self.model_details = LogObject(branches, results, options)
         self.entries = []
 
     def make_entry(self):
@@ -53,13 +57,29 @@ class Log:
 
         self.entries.append(new_log)
 
-    def get_summary(self):
-        pass
-
     def __repr__(self):
         logs = self.entries.__repr__().replace('[', '').replace(']', '').replace(', ', '\n')
         if len(logs) == 0:
             logs = "None"
 
-        return f"me3cs Models created:\n" \
+        return f"log entries:\n" \
                f"{logs}"
+
+    def get_summary(self) -> dict:
+
+        pass
+
+
+def extract_values(lst, *keys):
+    """
+    Given a list of dictionaries, extract the values corresponding to the given keys.
+
+    Args:
+        lst (list[dict]): A list of dictionaries.
+        keys (str): The keys to extract values for.
+
+    Returns:
+        list: A list of tuples, where each tuple contains the values corresponding
+        to the given keys in each dictionary.
+    """
+    return [tuple(d.get(key) for key in keys) for d in lst]
