305c831692057f9f597c0fc6ec6d912a2114c02c
DanielHoj <danielhoj1990@gmail.com>
2023-04-14 17:11:37 +0200

    Added set_model_from_log

--- me3cs/framework/log.py
+++ me3cs/framework/log.py
@@ -1,11 +1,14 @@
 import datetime
 from copy import deepcopy
+import typing
 
 from me3cs.framework.helper_classes.options import Options
-
 from me3cs.framework.branch import Branch
 from me3cs.framework.results import Results
 from me3cs.preprocessing.called import Called
+
+if typing.TYPE_CHECKING:
+    from me3cs.framework.base_model import BaseModel
 
 
 class LogObject:
@@ -35,9 +38,10 @@ class LogObject:
         self.rows = rows
         self.created_at = datetime.datetime.now().replace(microsecond=0)
         self.last_model_called = None
+        self.comment = None
 
     def __repr__(self) -> str:
-        return f"model type: {self.last_model_called} created {self.created_at}"
+        return f"model type: {self.last_model_called} - created {self.created_at}"
 
     def __copy__(self):
         new_obj = LogObject(self._prep, self._missing_data, self.results, self.options, self.rows)
@@ -53,21 +57,69 @@ class LogObject:
         new_obj.last_model_called = self.last_model_called
         return new_obj
 
+    def add_comment(self, comment: [None, str]):
+        if not (isinstance(comment, str) or comment is None):
+            raise TypeError("comment needs to be a string")
+        self.comment = comment
+
 
 class Log:
-    def __init__(self, branches: [list[Branch, Branch], list[Branch]], results: Results, options: Options):
+    def __init__(self, model: "BaseModel", branches: [list[Branch, Branch], list[Branch]], results: Results, options: Options):
 
         prep = tuple(prep.preprocessing.called for prep in branches)
         missing_data = tuple(missing.missing_data.called for missing in branches)
-
         rows = branches[0]._row_index
+
+        self._model = model
         self.log_object = LogObject(prep, missing_data, results, options, rows)
         self.entries = []
 
-    def make_entry(self):
+    def make_entry(self, comment: [str, None] = None) -> None:
         new_log = deepcopy(self.log_object)
-
+        new_log.add_comment(comment)
         self.entries.append(new_log)
+
+    def set_model_from_log(self, entry_number: int) -> None:
+
+        if not isinstance(entry_number, int):
+            raise TypeError("Please input an int as entry_number")
+
+        model_entry = self.entries[entry_number]
+
+        self._model.results = model_entry.results
+        self._model.options = model_entry.options
+
+        if not self._model._single_branch:
+            # set called
+            self._model.x.preprocessing.called = model_entry.x_prep
+            self._model.x.missing_data.called = model_entry.x_missing
+
+            self._model.y.preprocessing.called = model_entry.y_prep
+            self._model.y.missing_data.called = model_entry.y_missing
+
+            self._model.x._row_index = model_entry.rows
+            self._model.y._row_index = model_entry.rows
+
+            # call in order
+            self._model.x._reset_link()
+            self._model.x.missing_data.call_in_order()
+            self._model.x.preprocessing.call_in_order()
+
+            self._model.y.missing_data.call_in_order()
+            self._model.y.preprocessing.call_in_order()
+
+        else:
+            # Set called
+            self._model.x.preprocessing.called = model_entry.x_prep
+            self._model.x.missing_data.called = model_entry.x_missing
+            self._model.x._row_index = model_entry.rows
+
+            # Call in order
+            self._model.x._reset_link()
+            self._model.x.missing_data.call_in_order()
+            self._model.x.preprocessing.call_in_order()
+
+        self._model.x._update_data_from_index()
 
     def __repr__(self):
         logs = self.entries.__repr__().replace('[', '').replace(']', '').replace(', ', '\n')
@@ -80,18 +132,3 @@ class Log:
     def get_summary(self) -> dict:
 
         pass
-
-
-def extract_values(lst, *keys):
-    """
-    Given a list of dictionaries, extract the values corresponding to the given keys.
-
-    Args:
-        lst (list[dict]): A list of dictionaries.
-        keys (str): The keys to extract values for.
-
-    Returns:
-        list: A list of tuples, where each tuple contains the values corresponding
-        to the given keys in each dictionary.
-    """
-    return [tuple(d.get(key) for key in keys) for d in lst]
