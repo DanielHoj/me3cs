ddae63df7694fa7bfa3c21d4d434b61cf1c93515
DanielHoj <danielhoj1990@gmail.com>
2023-04-25 22:48:25 +0200

    Changed to data functionality

--- me3cs/framework/outlier_detection.py
+++ me3cs/framework/outlier_detection.py
@@ -9,24 +9,9 @@ if typing.TYPE_CHECKING:
 
 class OutlierDetection:
     def __init__(self, model: "BaseModel"):
-        self._linked_branches = model._linked_branches
+        self._branches = model.branches
         self._result = model.results
         self._model = model
-
-    def remove_outlier(self, index: [int, list[int], tuple[int], np.ndarray]):
-        index_total = self._linked_branches.get_rows()["_preprocessing_data_link"].copy()
-        false_index = count_false(index_total)
-        if isinstance(index, int):
-            updated_index = index_checker_int(false_index, index)
-            index_total[updated_index] = False
-        elif isinstance(index, (tuple, list, np.ndarray)):
-            updated_index = index_checker_tuple(false_index, index)
-            for i in updated_index:
-                index_total[i] = False
-        else:
-            raise TypeError("Input should be an integer or a tuple")
-        self._linked_branches.set_all_rows("_preprocessing_data_link", index_total)
-        self._linked_branches.call_preprocessing_in_order()
 
     def _remove_outlier_from(self, number_of_outliers_to_remove: int, diagnostic_name: str):
         if self._result.diagnostics is None:
@@ -41,7 +26,8 @@ class OutlierDetection:
         diagnostic_optimal = diagnostic[:, self._result.optimal_number_component]
         diagnostic_optimal = diagnostic_optimal.argsort()
         outliers_to_remove = diagnostic_optimal[-number_of_outliers_to_remove:]
-        self.remove_outlier(outliers_to_remove)
+        [branch.data_class.remove_rows("outlier_detection", outliers_to_remove) for branch in self._branches]
+        [branch.preprocessing.call_in_order() for branch in self._branches]
 
     def remove_outlier_from_q_residuals(self, number_of_outliers_to_remove: int = 1):
         self._remove_outlier_from(number_of_outliers_to_remove, "q_residuals")
@@ -53,7 +39,7 @@ class OutlierDetection:
         self._remove_outlier_from(number_of_outliers_to_remove, "leverage")
 
     def reset(self):
-        self._linked_branches.reset_to_link("_missing_data_link")
+        [branch.data_class.reset_index("outlier_detection") for branch in self._branches]
 
 
 def index_checker_tuple(existing: tuple, new: tuple) -> list:
