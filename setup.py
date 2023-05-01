d379f93c13802f90f40f7dc4e52ef39130f965b9
DanielHoj <danielhoj1990@gmail.com>
2023-05-01 19:26:04 +0200

    moved all images to one folder

--- setup.py
+++ setup.py
@@ -3,12 +3,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='me3cs',
-    version='0.1.3',
+    version='0.1.6',
     license='BSD 3-clause',
     author='Daniel Andreas Njust Høj',
     author_email='d.njust.hoej@gmail.com',
-    packages=find_packages('me3cs'),
-    package_dir={'': 'me3cs'},
+    packages=find_packages(),
     url='https://github.com/DanielHoj/me3cs',
     keywords='chemometrics',
     install_requires=["pandas",
